e3026f438cf717d8642c5655202d3649af6ff7da
Johno06 <johnosmond0506@gmail.com>
2023-09-23 19:18:18 -0600

    latest

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -6,18 +6,19 @@ import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Member, MemberRole, Profile } from "@prisma/client";
-import { UserAvatar } from "@/components/user-avatar";
-import { ActionTooltip } from "@/components/action-tooltip";
 import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
+
+import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
 import { cn } from "@/lib/utils";
 import {
     Form,
     FormControl,
     FormField,
-    FormItem
+    FormItem,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -27,9 +28,9 @@ interface ChatItemProps {
     id: string;
     content: string;
     member: Member & {
-        profile: Profile
+        profile: Profile;
     };
-    timestamp: string; 
+    timestamp: string;
     fileUrl: string | null;
     deleted: boolean;
     currentMember: Member;
@@ -40,7 +41,7 @@ interface ChatItemProps {
 
 const roleIconMap = {
     "GUEST": null,
-    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500"/>,
+    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
     "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
 }
 
@@ -58,7 +59,7 @@ export const ChatItem = ({
     currentMember,
     isUpdated,
     socketUrl,
-    socketQuery,
+    socketQuery
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false);
     const { onOpen } = useModal();
@@ -66,7 +67,7 @@ export const ChatItem = ({
     const router = useRouter();
 
     const onMemberClick = () => {
-        if(member.id === currentMember.id){
+        if (member.id === currentMember.id) {
             return;
         }
 
@@ -75,18 +76,19 @@ export const ChatItem = ({
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
-            if(event.key === "Escape" || event.keyCode === 27){
+            if (event.key === "Escape" || event.keyCode === 27) {
                 setIsEditing(false);
             }
         };
 
         window.addEventListener("keydown", handleKeyDown);
+
         return () => window.removeEventListener("keyDown", handleKeyDown);
-    })
+    }, []);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-        defaultValues:{
+        defaultValues: {
             content: content
         }
     });
@@ -103,7 +105,7 @@ export const ChatItem = ({
             await axios.patch(url, values);
 
             form.reset();
-            setIsEditing(false)
+            setIsEditing(false);
         } catch (error) {
             console.log(error);
         }
@@ -125,11 +127,11 @@ export const ChatItem = ({
     const isPDF = fileType === "pdf" && fileUrl;
     const isImage = !isPDF && fileUrl;
 
-    return(
+    return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
                 <div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
-                    <UserAvatar src={member.profile.imageUrl}/>
+                    <UserAvatar src={member.profile.imageUrl} />
                 </div>
                 <div className="flex flex-col w-full">
                     <div className="flex items-center gap-x-2">
@@ -146,16 +148,29 @@ export const ChatItem = ({
                         </span>
                     </div>
                     {isImage && (
-                        <a href={fileUrl}
-                        target="_blank" rel="noopener noreferrer"
-                        className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48">
-                            <Image src={fileUrl} alt={content} fill className="object-cover"/>
+                        <a
+                            href={fileUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48"
+                        >
+                            <Image
+                                src={fileUrl}
+                                alt={content}
+                                fill
+                                className="object-cover"
+                            />
                         </a>
                     )}
                     {isPDF && (
-                        <div className="relative flex items-center p-2 mt-2 rounded-md bg-backgroud/10">
+                        <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
                             <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-                            <a href={fileUrl} target="_blank" rel="noopener noreferrer" className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline">
+                            <a
+                                href={fileUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+                            >
                                 PDF File
                             </a>
                         </div>
@@ -175,19 +190,27 @@ export const ChatItem = ({
                     )}
                     {!fileUrl && isEditing && (
                         <Form {...form}>
-                            <form className="flex items-center w-full gap-x-2 pt-2" onSubmit={form.handleSubmit(onSubmit)}>
-                                <FormField control={form.control} name="content" render={({field}) => (
-                                    <FormItem className="flex-1">
-                                        <FormControl>
-                                            <div className="relative w-full">
-                                                <Input
+                            <form
+                                className="flex items-center w-full gap-x-2 pt-2"
+                                onSubmit={form.handleSubmit(onSubmit)}>
+                                <FormField
+                                    control={form.control}
+                                    name="content"
+                                    render={({ field }) => (
+                                        <FormItem className="flex-1">
+                                            <FormControl>
+                                                <div className="relative w-full">
+                                                    <Input
                                                         disabled={isLoading}
-                                                         className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
-                                                        placeholder="Edited Message" {...field}/>
-                                            </div>
-                                        </FormControl>
-                                    </FormItem>
-                                )}/>
+                                                        className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                                                        placeholder="Edited message"
+                                                        {...field}
+                                                    />
+                                                </div>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                                 <Button disabled={isLoading} size="sm" variant="primary">
                                     Save
                                 </Button>
@@ -203,11 +226,20 @@ export const ChatItem = ({
                 <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
                     {canEditMessage && (
                         <ActionTooltip label="Edit">
-                            <Edit onClick={() => setIsEditing(true)} className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"/>
+                            <Edit
+                                onClick={() => setIsEditing(true)}
+                                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+                            />
                         </ActionTooltip>
                     )}
                     <ActionTooltip label="Delete">
-                        <Trash onClick={() => onOpen("deleteMessage", {apiUrl: `${socketUrl}/${id}`, query: socketQuery,})} className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+                        <Trash
+                            onClick={() => onOpen("deleteMessage", {
+                                apiUrl: `${socketUrl}/${id}`,
+                                query: socketQuery,
+                            })}
+                            className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+                        />
                     </ActionTooltip>
                 </div>
             )}
