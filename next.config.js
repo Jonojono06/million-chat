73add3558ac8aa1d49e3c4a978fb67040bc7113f
Johno06 <johnosmond0506@gmail.com>
2023-10-04 14:47:47 -0600

    Added PWA (deleted capacitor)

--- next.config.js
+++ next.config.js
@@ -1,19 +1,118 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     webpack: (config) => {
+//         config.externals.push ({
+//             "utf-8-validate": "commonjs utf-8-validate",
+//             bufferutil: "commonjs bufferutil"
+//         });
+
+//         return config;
+//     },
+//     images: {
+//         domains: [
+//             "utfs.io"
+//         ]
+//     }
+// }
+
+// module.exports = nextConfig
+
+
+// const withPWA = require ('next-pwa');
+
+// const nextConfig = {
+//   webpack: config => {
+//     config.externals.push ({
+//       'utf-8-validate': 'commonjs utf-8-validate',
+//       bufferutil: 'commonjs bufferutil',
+//     });
+
+//     return config;
+//   },
+//   images: {
+//     domains: ['utfs.io'],
+//   },
+// };
+
+// module.exports = withPWA (nextConfig, ({
+//      ...withPWA ({
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   }),
+// }));
+
+// const withPWA = require ('next-pwa');
+
+// const nextConfig = {
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   webpack: config => {
+//     // Consider commenting out this part if it's not necessary
+//     config.externals.push ({
+//       'utf-8-validate': 'commonjs utf-8-validate',
+//       bufferutil: 'commonjs bufferutil',
+//     });
+
+//     return config;
+//   },
+//   images: {
+//     domains: ['utfs.io'],
+//   },
+// };
+
+// module.exports = withPWA (nextConfig);
+
+// const withPWA = require ('next-pwa') ({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+// });
+
+// module.exports = withPWA (
+//   {
+//     webpack: config => {
+//     // Consider commenting out this part if it's not necessary
+//     config.externals.push ({
+//       'utf-8-validate': 'commonjs utf-8-validate',
+//       bufferutil: 'commonjs bufferutil',
+//     });
+
+//     return config;
+//   },
+//   images: {
+//     domains: ['utfs.io'],
+//   },
+//   }
+// );
+
+
+
+const withPWA = require ('next-pwa') ({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
+});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    webpack: (config) => {
-        config.externals.push ({
-            "utf-8-validate": "commonjs utf-8-validate",
-            bufferutil: "commonjs bufferutil"
-        });
+    webpack: config => {
+    config.externals.push ({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
 
-        return config;
-    },
-    images: {
-        domains: [
-            "utfs.io"
-        ]
-    }
-}
+    return config;
+  },
+  images: {
+    domains: ['utfs.io'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA (nextConfig);
+
+
+
