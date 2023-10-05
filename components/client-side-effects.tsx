d5beabae597eef94340ab57354e2b8cb21d2ba02
Johno06 <johnosmond0506@gmail.com>
2023-10-05 13:39:10 -0600

    added splashscreen

--- components/client-side-effects.tsx
+++ components/client-side-effects.tsx
@@ -1,26 +1,26 @@
-"use client";
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+// "use client";
+// import { useTheme } from 'next-themes';
+// import { useEffect, useState } from 'react';
 
-export default function ClientSideEffects() {
-    const { theme } = useTheme();
-    const [currentThemeColor, setCurrentThemeColor] = useState('#313338'); // initial value
+// export default function ClientSideEffects() {
+//     const { theme } = useTheme();
+//     const [currentThemeColor, setCurrentThemeColor] = useState('#313338'); // initial value
 
-    useEffect(() => {
-        if (theme === 'dark') {
-            setCurrentThemeColor('#313338'); // Dark mode color
-        } else {
-            setCurrentThemeColor('#fff'); // Light mode color
-        }
-    }, [theme]);
+//     useEffect(() => {
+//         if (theme === 'dark') {
+//             setCurrentThemeColor('#313338'); // Dark mode color
+//         } else {
+//             setCurrentThemeColor('#fff'); // Light mode color
+//         }
+//     }, [theme]);
 
-    // Dynamically set the meta tag for theme color
-    useEffect(() => {
-        const metaThemeColor = document.querySelector("meta[name=theme-color]");
-        if (metaThemeColor) {
-            metaThemeColor.setAttribute("content", currentThemeColor);
-        }
-    }, [currentThemeColor]);
+//     // Dynamically set the meta tag for theme color
+//     useEffect(() => {
+//         const metaThemeColor = document.querySelector("meta[name=theme-color]");
+//         if (metaThemeColor) {
+//             metaThemeColor.setAttribute("content", currentThemeColor);
+//         }
+//     }, [currentThemeColor]);
 
-    return null;
-}
+//     return null;
+// }
