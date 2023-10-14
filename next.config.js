2338fe085b25aaa21d90e49f591a1cf369b90eb0
Johno06 <johnosmond0506@gmail.com>
2023-10-13 22:45:03 -0600

    added sw path

--- next.config.js
+++ next.config.js
@@ -95,6 +95,7 @@ const withPWA = require ('next-pwa') ({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  sw: "/sw.js",
 //   disable: process.env.NODE_ENV === 'development',
 });
 const withBundleAnalyzer = require ('@next/bundle-analyzer') ({
